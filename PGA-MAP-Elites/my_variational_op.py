69ebe503f6aab1b45219bdcb09dd4458155d66f6
tsukii7 <1375926565@qq.com>
2022-11-26 02:13:50 +0800

    Revert "add PG"
    This reverts commit 3b616095affb7a5ad17804eeddd98b2be581c4d3.

--- PGA-MAP-Elites/my_variational_op.py
+++ PGA-MAP-Elites/my_variational_op.py
@@ -3,20 +3,16 @@ import copy
 import numpy as np
 import torch
 
+
 def variation(archive,
               batch_size,
-              proportion_evo,
-              learning_rate,
-              critic,
-              states,
-              nr_of_steps_act):
-
+              proportion_evo):
     keys = list(archive.keys())
 
     actors_z = []
     actors_x_evo = []
     actors_y_evo = []
-    # GA: crossover
+
     # 产生actor父母
     rand_evo_1 = np.random.randint(len(keys), size=int(batch_size * proportion_evo))
     rand_evo_2 = np.random.randint(len(keys), size=int(batch_size * proportion_evo))
@@ -30,31 +26,9 @@ def variation(archive,
         actor_z = copy.deepcopy(actor_x)
         actor_z_state_dict = crossover(actor_x.state_dict(), actor_y.state_dict())  # 将actor x和y的网络参数crossover
         actor_z.load_state_dict(actor_z_state_dict)
-        actors_z += actor_z
-
-    # PG
-    actors_x_grad = []
-    rand_grad = np.random.randint(len(keys), size=(batch_size - int(batch_size * proportion_evo)))
-    for n in range(0, len(rand_grad)):
-        actors_x_grad += [archive[keys[rand_grad[n]]]]  # 取individual
-
-    # 输入所有actor，一个critic， states，
-    for individual in actors_x_grad:
-        actor_z = individual.x
-        actor_z_copy = copy.deepcopy(actor_z)
-        for param in actor_z_copy.parameters():
-            param.requires_grad = True
-        optimizer = torch.optim.Adam(actor_z_copy.parameters(), lr=learning_rate)
-        for i in range(nr_of_steps_act):
-            state = states[i]
-            actor_loss = -critic.Q1(state, actor_z(state)).mean()
-            optimizer.zero_grad()
-            actor_loss.backward()
-            optimizer.step()
-        for param in actor_z.parameters():
-            param.requires_grad = False
         actors_z.append(actor_z)
     return actors_z
+
 
 def crossover(actorx_state_dict, actory_state_dict):
     actor_z_state_dict = copy.deepcopy(actorx_state_dict)
